e324e375d0cbb344ea66a6af00548ab8e4b9415b
TomAllison067 <tomlaurenceallison@gmail.com>
2021-01-18 20:14:10 +0000

    Added a test case to ensure a connection can be made to a sqlite database.

--- modulesApplication/tests.py
+++ modulesApplication/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.db import connections
+
 
 # Create your tests here.
+class TestSQLiteDatabaseConnection(TestCase):
+    """
+    Tests that a connection can be made to a SQLite database.
+    """
+    def test_sqlite_database_connection(self):
+        db_conn = connections['default']
+        c = db_conn.cursor()
+        self.assertIsNotNone(c, "The database cursor is None if there is no connection.")
