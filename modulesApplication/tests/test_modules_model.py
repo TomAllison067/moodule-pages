04351c25b322eee01bd5553b0475375cbe96bf3d
TomAllison067 <tomlaurenceallison@gmail.com>
2021-01-21 11:21:49 +0000

    Added a test case ensuring a new empty database returns no models.

--- modulesApplication/tests/test_modules_model.py
+++ modulesApplication/tests/test_modules_model.py
@@ -4,15 +4,25 @@ from modulesApplication.models import Module
 
 
 class TestModulesModel(TestCase):
+    def setUp(self):
+        self.module1 = Module(mod_code="CS2815", title="Small Enterprise Team Project", year=2, department="CS",
+                              contact_hours=40, exams=0, practical=0, coursework=100, credits=15,
+                              prerequisites="CS1813; CS2800", summary="This is a test summary",
+                              outcomes="To do TDD in our project", status="ACTIVE")
+
+    def test_no_module(self):
+        """
+        Tests that no modules exist in a new, empty database.
+        """
+        q = Module.objects.filter(pk="CS2815")
+        self.assertEqual(q.count(), 0, "There should be no models in the database until one is saved.")
+
     def test_new_module(self):
         """
         Tests that a new module can be created, saved in the database, and then queried.
         """
-        m = Module(mod_code="CS2815", title="Small Enterprise Team Project", year=2, department="CS",
-                   contact_hours=40, exams=0, practical=0, coursework=100, credits=15,
-                   prerequisites="CS1813; CS2800", summary="This is a test summary",
-                   outcomes="To do TDD in our project", status="ACTIVE")
+        m = self.module1
         m.save()
-        m1 = Module.objects.filter(pk="CS2815").first()
-        self.assertIsNotNone(m1, "The newly created Module object is null.")
-        self.assertEqual("CS2815", m1.mod_code)
+        q = Module.objects.filter(pk="CS2815").first()
+        self.assertIsNotNone(q, "The newly created Module object is null.")
+        self.assertEqual("CS2815", q.mod_code)
