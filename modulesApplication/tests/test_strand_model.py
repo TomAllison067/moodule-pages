728879b0018684c06e351797b06f74bcb524d1e2
vellita <vel.rod1110@gmail.com>
2021-01-26 11:03:07 +0000

    Added a test case to query from the database and checking values against Strand objects

--- modulesApplication/tests/test_strand_model.py
+++ modulesApplication/tests/test_strand_model.py
@@ -21,14 +21,14 @@ class MyTestCase(TransactionTestCase):
         self.newstrand.save()
         self.assertEqual(Strands.objects.count(), 1, "Strand added to database, total number of records should be 1")
 
-    def test_duplicates(self):
+    def test_no_duplicates(self):
         """
         Test to check the database will not store duplicate values
         """
         self.newstrand.save()
         self.assertEqual(Strands.objects.count(), 1, "one record added ")
         same_strand = self.newstrand
-        same_strand.save()
+        same_strand.save()  # trying to save same object
         self.assertEqual(Strands.objects.count(), 1, "if same record attempted to be inserted, it will not be added")
 
     def test_same_code_diff_strand(self):
@@ -38,8 +38,25 @@ class MyTestCase(TransactionTestCase):
         self.newstrand.save()
         another_strand = Strands(mod_code=Module.objects.first(), strand='HH')
         another_strand.save()
-        self.assertEqual(self.newstrand.strand, Strands.objects.first().strand)
+
         self.assertEqual(Strands.objects.count(), 2, "if there is 2 object with the same mod_code but different strand")
+        self.assertNotEqual(Strands.objects.first().strand, Strands.objects.filter(mod_code='CS2815').last().strand)
 
+    def test_db_query(self):
+        """
+        Test to query and access values in the database
+        """
+        self.newstrand.save()  # saving strand created in setup
 
+        Strands(mod_code=Module.objects.first(), strand='HH').save()  # saving new strand object, total number of record should be 2
 
+        new_module = Module(mod_code='CS7432')  # new Module object
+        new_module.save()  # saving new Module
+        third_strand = Strands(mod_code=new_module, strand='CM')  # new stand object
+        third_strand.save()  # saving a new strand from the new module, total records should be 3 now
+
+        self.assertEqual(Strands.objects.count(), 3, 'added 3 strands to the database')
+        # testing if the object entered is the same as the object in database
+        self.assertEqual(Strands.objects.filter(mod_code='CS7432').first(), third_strand)
+        # test querying the database to get the strand value of a specific object
+        self.assertEqual(Strands.objects.filter(mod_code='CS7432').first().strand, 'CM')
