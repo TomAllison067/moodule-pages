6d3e728c9e809c4febebd26c6f5c1a668df9ffe8
KPetkova48 <zhac099@live.rhul.ac.uk>
2021-01-27 13:36:49 +0000

    Adding additional functionality to the allModules page

--- modulesApplication/urls.py
+++ modulesApplication/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('AllModules.html', views.index, name='index'),
     path('StudentLandingPage.html', views.studentLanding, name='studentLanding'),
     path('StudentChooseModules.html', views.studentChooseModules, name='studentChooseModules'),
+    path('ReadMore.html', views.readMore, name='ReadMore'),
 ]
