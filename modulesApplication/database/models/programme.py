d6c12776a7165bba9cf4a9e183f58a7c61c3b2ab
vellita <vel.rod1110@gmail.com>
2021-02-01 21:29:46 +0000

    Added comments to help others understand how we use test cases to create tables

--- modulesApplication/database/models/programme.py
+++ modulesApplication/database/models/programme.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class Programme(models.Model):
+    """
+    to help create model, you can use the extracted models from the sqlite database(file location below)
+    need to find the model you require, in this case 'Programmes'
+    use it as a reference, create a new file in the database/model folder, create model with the fields required
+    be sure to check attributes of fields e.g check the null fields, make sure there is a primary key etc.
+
+    file location: modulesApplication/resources/develop/models.py
+    """
     prog_code = models.TextField(primary_key=True)
     title = models.TextField()
     level = models.TextField()
--- modulesApplication/tests/test_programmes_model.py
+++ modulesApplication/tests/test_programmes_model.py
@@ -2,9 +2,19 @@ from django.db import IntegrityError
 from django.test import TransactionTestCase
 
 from modulesApplication.models import Programme
+"""
+To use the table we need to import the model required, in this case 'Programmes',
+if using foreign keys or querying from multiple table, be sure to import these models.
+file location: modulesApplication/models.py
+"""
 
 
 class TestProgrammes(TransactionTestCase):
+    """
+    writing test to aid our development of the Programmes table
+
+    each testcase must start with 'test' eg test_new_feature or testNewFeature etc.
+    """
     def test_one_programme_saved(self):
         Programme.objects.create(prog_code="1067", title="BSc Computer Science", level="BSC")
         self.assertEqual(1, Programme.objects.count(), "One programme saves successfully.")
@@ -17,3 +27,16 @@ class TestProgrammes(TransactionTestCase):
         Programme.objects.create(prog_code="1067", title="BSc Computer Science", level="BSC")
         Programme.objects.create(prog_code="1059", title="BSc Computer Science (Artificial Intelligence)", level="BSC")
         self.assertEqual(2, Programme.objects.count(), "Two programmes save successfully.")
+
+
+"""
+As you write and run tests you will need to make changes to the model dependant on how you require the database
+(basically TDD)
+e.g looking at the Programmes database I might decide to have a composite key of prog_code and title
+    then we'd have to edit the 'Meta' of the model to the a unique_together, 
+    example of this is in the strands model(modulesApplication/database/models/strand.py)
+    
+use other models(from /database/models) and tests(from /tests) as a guide to creating tables 
+
+command to run test from specific file: python manage.py test modulesApplication.tests.test_programmes_model
+"""
