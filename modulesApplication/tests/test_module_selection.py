7b600b73472e98b0a0fef86a9366a418168f3e50
TomAllison067 <tomlaurenceallison@gmail.com>
2021-02-11 20:09:40 +0000

    moduleSelectionModel: Modules now have a many to many relationship with ModuleSelections

--- modulesApplication/tests/test_module_selection.py
+++ modulesApplication/tests/test_module_selection.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from modulesApplication.models import ModuleSelection
+from modulesApplication.models import ModuleSelection, Module
 
 
 class TestModuleSelection(TestCase):
@@ -10,3 +10,12 @@ class TestModuleSelection(TestCase):
         self.assertEqual('1234', selection.student_id)
         self.assertEqual('1', selection.stage)
         self.assertEqual('PENDING', selection.status)
+
+    def test_modules(self):
+        selection = ModuleSelection.objects.create(student_id='1234', stage='1', status='PENDING')
+        m1 = Module.objects.create(mod_code='cs1234', title='foo')
+        m2 = Module.objects.create(mod_code='cs4567', title='bar')
+        m1.selections.add(selection)
+        m2.selections.add(selection)
+        selected = selection.module_set.all()
+        self.assertEqual(set(Module.objects.all()), set(selected))
