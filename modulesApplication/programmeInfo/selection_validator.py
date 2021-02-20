3612d0b6e55f102c240d5c0fffe03a066e088752
TomAllison067 <tomlaurenceallison@gmail.com>
2021-02-20 23:29:49 +0000

    selection validation: Added simple (failing) test case for SelectionValidator. Mock data and validation logic still TODO.

--- modulesApplication/programmeInfo/selection_validator.py
+++ modulesApplication/programmeInfo/selection_validator.py
@@ -2,5 +2,17 @@ from modulesApplication.models import ModuleSelection
 
 
 class SelectionValidator:
+    """
+    A SelectionValidator can be used to validate whether a given ModuleSelection is valid or not.
+    """
+
     def __init__(self, selection: ModuleSelection):
         self._selection = selection
+
+    def validate(self) -> bool:
+        """
+        Evaluate this validator's ModuleSelection.
+        :return: True if the modules selected are valid for the student's degree, entry year and stage.
+        False otherwise.
+        """
+        pass  # TODO: Not yet implemented.
--- modulesApplication/tests/unit/test_selection_validator.py
+++ modulesApplication/tests/unit/test_selection_validator.py
@@ -1,8 +1,24 @@
 from django.test import TestCase
 
-from modulesApplication.models import ModuleSelection
+from modulesApplication.models import ModuleSelection, Programme
+from modulesApplication.programmeInfo.selection_validator import SelectionValidator
 
 
 class TestSelectionValidator(TestCase):
     def test_simple_selection_is_valid(self):
-        selection = ModuleSelection
+        programme = Programme.objects.create(prog_code="123", level="BSc")
+        selection = ModuleSelection.objects.create(
+            student_id="foo",
+            stage="1",
+            entry_year="2019",
+            status="PENDING",
+            programme=programme
+        )
+        # TODO: Add stub modules and have them selected by this validator
+        pass
+
+        # TODO: Add option rules linking stub programme to stub modules
+        pass
+
+        validator = SelectionValidator(selection)
+        self.assertTrue(validator.validate(), "Not yet implemented.")
