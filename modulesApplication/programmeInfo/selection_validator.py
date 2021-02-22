09bf9861ee0bf3a20365e6479e66cf58a637a556
TomAllison067 <tomlaurenceallison@gmail.com>
2021-02-22 13:13:31 +0000

    selection_validation: Added test case to pass simple core rule validation - i.e, stage 1 with no DISC_ALT

--- modulesApplication/programmeInfo/selection_validator.py
+++ modulesApplication/programmeInfo/selection_validator.py
@@ -1,4 +1,4 @@
-from modulesApplication.models import ModuleSelection
+from modulesApplication.models import ModuleSelection, OptionRule
 
 
 class SelectionValidator:
@@ -8,6 +8,20 @@ class SelectionValidator:
 
     def __init__(self, selection: ModuleSelection):
         self._selection = selection
+        self._rules = self.get_rules()
+        self._modules_selected = set([m.mod_code for m in self._selection.module_set.all()])
+
+    def get_rules(self):
+        query = OptionRule.objects.filter(
+            prog_code=self._selection.programme,
+            stage=self._selection.stage,
+            entry_year=self._selection.entry_year,
+        )
+        rules = {}
+        for rule in query:
+            rules[rule.constraint_type] = rules.get(rule.constraint_type, [])
+            rules[rule.constraint_type].append(rule)
+        return rules
 
     def validate(self) -> bool:
         """
@@ -15,4 +29,13 @@ class SelectionValidator:
         :return: True if the modules selected are valid for the student's degree, entry year and stage.
         False otherwise.
         """
-        pass  # TODO: Not yet implemented.
+        return self.validate_core_rules()
+
+    def validate_core_rules(self) -> bool:
+        count = 0
+        core_rule = self._rules['CORE'][0]
+        patterns = set(core_rule.mod_code_pattern.split(","))
+        for mod_code in self._modules_selected:
+            if mod_code in patterns:
+                count += 1
+        return core_rule.min_quantity <= count <= core_rule.max_quantity
--- modulesApplication/tests/unit/test_selection_validator.py
+++ modulesApplication/tests/unit/test_selection_validator.py
@@ -1,24 +1,49 @@
 from django.test import TestCase
 
-from modulesApplication.models import ModuleSelection, Programme
+from modulesApplication.models import ModuleSelection, Programme, Module, OptionRule
 from modulesApplication.programmeInfo.selection_validator import SelectionValidator
 
 
 class TestSelectionValidator(TestCase):
     def test_simple_selection_is_valid(self):
-        programme = Programme.objects.create(prog_code="123", level="BSc")
-        selection = ModuleSelection.objects.create(
+        """Assert that a simple made up stage 1 selection is correct - i.e, core modules."""
+        p1 = Programme.objects.create(prog_code="p1", level="BSc")
+        valid_selection = ModuleSelection.objects.create(
             student_id="foo",
+            stage=1,
+            entry_year="2019",
+            status="PENDING",
+            programme=p1
+        )
+        invalid_selection = ModuleSelection.objects.create(
+            student_id="bar",
             stage="1",
             entry_year="2019",
             status="PENDING",
-            programme=programme
+            programme=p1
         )
-        # TODO: Add stub modules and have them selected by this validator
-        pass
+        # Add stub modules and have them selected by this validator
+        cm1 = Module.objects.create(mod_code="m1", title="module1")
+        cm2 = Module.objects.create(mod_code="m2", title="module2")
 
-        # TODO: Add option rules linking stub programme to stub modules
-        pass
+        # Add option rules linking stub programme to stub modules
+        OptionRule.objects.create(
+            prog_code=p1,
+            entry_year='2019',
+            stage=1,
+            constraint_type="CORE",
+            min_quantity=2,
+            max_quantity=2,
+            mod_code_pattern="m1,m2"
+        )
 
-        validator = SelectionValidator(selection)
-        self.assertTrue(validator.validate(), "Not yet implemented.")
+        # The valid selection contains both core modules.
+        cm1.selected_in.add(valid_selection)
+        cm2.selected_in.add(valid_selection)
+
+        # The invalid selection contains only one of the core modules.
+        validator = SelectionValidator(valid_selection)
+        self.assertTrue(validator.validate(), "A valid selection should return True.")
+
+        validator = SelectionValidator(invalid_selection)
+        self.assertFalse(validator.validate(), "An invalid selection should return False.")
