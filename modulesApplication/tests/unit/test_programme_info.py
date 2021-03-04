b4b57dc45b9bcb952d239018d67c443b6dc4ee8f
TomAllison067 <tomlaurenceallison@gmail.com>
2021-03-04 15:55:40 +0000

    bugfixes: Removed redundant unit test for the correct number of stages in ProgrammeInfo. ProgrammeInfo now only returns information for one provided stage.

--- modulesApplication/tests/unit/test_programme_info.py
+++ modulesApplication/tests/unit/test_programme_info.py
@@ -17,29 +17,6 @@ class TestProgrammeInfo(TestCase):
         expected = Programme.objects.get(prog_code='foo')
         self.assertEqual(expected, p.programme)
 
-    def test_stage(self):
-        """Test that a new ProgrammeInfo object is given the correct number of stages"""
-
-        # BSc
-        bsc = Programme.objects.create(prog_code='bsc', title='bsc', level='BSc', yini=0)
-        p = factory.get_programme_info(prog_code='bsc', entry_year='2019')
-        self.assertEqual(3, p.stages, "A standard BSc with no masters or YINI has three stages.")
-
-        # BSc (Year in Industry)
-        bsc_yini = Programme.objects.create(prog_code='bscyini', title='bscyini', level='BSc', yini=1)
-        p = factory.get_programme_info(prog_code='bscyini', entry_year='2019')
-        self.assertEqual(4, p.stages, "A BSc with a YINI has 4 stages.")
-
-        # MSCi
-        msci = Programme.objects.create(prog_code='msci', title='msci', level='msci', yini=0)
-        p = factory.get_programme_info(prog_code='msci', entry_year='2019')
-        self.assertEqual(4, p.stages, "An MSCi with no YINI has 4 stages.")
-
-        # MSCi YINI
-        msci_yini = Programme.objects.create(prog_code='msciyini', title='msciyini', level='msci', yini=1)
-        p = factory.get_programme_info(prog_code='msciyini', entry_year='2019')
-        self.assertEqual(5, p.stages, "An MSCi with YINI has 5 stages.")
-
     def test_entry_year(self):
         """Test that a new ProgrammeInfo object has the correct entry year."""
         p = Programme.objects.create(prog_code='madeup', level='BSc')
