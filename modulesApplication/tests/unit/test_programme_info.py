6e7ae8541bf387a60852614c335a3e5c336f856e
TomAllison067 <tomlaurenceallison@gmail.com>
2021-03-04 15:57:20 +0000

    bugfixes: Fixed remaining TestProgrammeInfo unit tests, taking into account changed factory arguments.

--- modulesApplication/tests/unit/test_programme_info.py
+++ modulesApplication/tests/unit/test_programme_info.py
@@ -12,14 +12,14 @@ class TestProgrammeInfo(TestCase):
 
     def test_programme_object(self):
         """Test that a new ProgrammeInfo is instantiated with the correct Programme object."""
-        programme = Programme.objects.create(prog_code='foo', level='bsc')
-        p = factory.get_programme_info(prog_code='foo', entry_year='2019')
+        Programme.objects.create(prog_code='foo', level='bsc')
+        p = factory.get_programme_info(prog_code='foo', entry_year='2019', stage=1)
         expected = Programme.objects.get(prog_code='foo')
         self.assertEqual(expected, p.programme)
 
     def test_entry_year(self):
         """Test that a new ProgrammeInfo object has the correct entry year."""
-        p = Programme.objects.create(prog_code='madeup', level='BSc')
-        p = factory.get_programme_info(prog_code='madeup', entry_year='2019')
+        Programme.objects.create(prog_code='madeup', level='BSc')
+        p = factory.get_programme_info(prog_code='madeup', entry_year='2019', stage=1)
         expected = '2019'
         self.assertEqual(expected, p.entry_year)
