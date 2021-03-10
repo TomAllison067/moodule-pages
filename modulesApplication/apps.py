795f14d88c8c87a38fd2055f01543f1194b862ea
TomAllison067 <tomlaurenceallison@gmail.com>
2021-03-10 19:21:53 +0000

    LoginSystemFeature: Added prompt to server start up for LDAP credentials as per Matt's suggestion.

--- modulesApplication/apps.py
+++ modulesApplication/apps.py
@@ -1,5 +1,12 @@
+import getpass
+
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class ModulesApplicationConfig(AppConfig):
     name = 'modulesApplication'
+
+    def ready(self):
+        settings.AUTH_LDAP_BIND_DN = getpass.getpass("Please enter LDAP username: ")
+        settings.AUTH_LDAP_BIND_PASSWORD = getpass.getpass("Please enter LDAP password: ")
