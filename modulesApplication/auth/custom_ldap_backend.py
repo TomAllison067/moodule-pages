9fdf8ad4c03f11f0ea7fd47e7d9d36f8c3aff04d
TomAllison067 <tomlaurenceallison@gmail.com>
2021-03-16 17:18:38 +0000

    Minor refactoring in custom_ldap_backend.py

--- modulesApplication/auth/custom_ldap_backend.py
+++ modulesApplication/auth/custom_ldap_backend.py
@@ -6,10 +6,7 @@ class CustomLDAPBackend(LDAPBackend):
     """
     A custom override of the LDAP backend. The justification is to derive group memberships upon login and then
     add the resulting Django user to the correct Django group.
-    Examples of extensionAttribute8 in LDAP are:
-        - Student (Undergraduate)
-        - Student (Postgraduate)
-        - Staff (Academic Staff)
+
     """
 
     def authenticate_ldap_user(self, ldap_user, password):
@@ -18,16 +15,23 @@ class CustomLDAPBackend(LDAPBackend):
             django_group = self.derive_group(user)
             if not django_group:
                 return None
-            user.groups.add(django_group)
-            print("User is in")
-            print([g.name for g in user.groups.all()])
+            user.groups.add(django_group)  # Add the user to the correct group
         return user
 
-    def derive_group(self, user):
+    @staticmethod
+    def derive_group(user):
         """Given the authenticated user, this searches the corresponding LDAP attributes to derive if the user is
         staff or a student and returns a string corresponding to a Django group in our application.
-        :param user the authenticated user
-        :return result a string corresponding to the Django group name"""
+
+        It is derived from extensionAttribute8 in the University's LDAP entries for people.
+
+        Examples of extensionAttribute8 in LDAP are:
+        - Student (Undergraduate)
+        - Student (Postgraduate)
+        - Staff (Academic Staff)
+        :param user the authenticated user object
+        :return the correct Django group, or None
+        """
         ldap_group_value = user.ldap_user.attrs.get('extensionAttribute8')[0].lower()
         if 'academic staff' in ldap_group_value or 'administrative staff' in ldap_group_value:
             return Group.objects.get(name='Staff')
@@ -35,3 +39,5 @@ class CustomLDAPBackend(LDAPBackend):
             return Group.objects.get(name='Students')
         else:
             return None
+
+
