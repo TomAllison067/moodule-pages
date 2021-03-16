27bcb84c51347181496b9d81d81b7b363cde7eee
TomAllison067 <tomlaurenceallison@gmail.com>
2021-03-16 17:02:25 +0000

    Added try/catch block on redirect url for dev users

--- modulesApplication/views/auth.py
+++ modulesApplication/views/auth.py
@@ -6,11 +6,14 @@ from django.shortcuts import redirect
 @login_required
 def login_redirect(request):
     """A view to handle redirecting upon login."""
-    user_group = Group.objects.get(user=request.user).name
-    if user_group == "Students":
-        return redirect('modulesApplication:student-landing')
-    elif user_group == "Staff":
-        return redirect('modulesApplication:office-landing')
-    else:
+    try:
+        user_group = Group.objects.get(user=request.user).name
+        if user_group == "Students":
+            return redirect('modulesApplication:student-landing')
+        elif user_group == "Staff":
+            return redirect('modulesApplication:office-landing')
+        else:
+            return redirect('modulesApplication:index')
+    except Group.DoesNotExist:
         return redirect('modulesApplication:index')
 
