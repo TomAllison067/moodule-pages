09d800b9261fbb09c5972d62ed8f95384326b63f
TomAllison067 <tomlaurenceallison@gmail.com>
2021-03-19 12:43:52 +0000

    Access control tests for module cruds

--- modulesApplication/tests/integration/test_access_control.py
+++ modulesApplication/tests/integration/test_access_control.py
@@ -3,7 +3,7 @@ from django.test import TestCase, Client, tag
 from django.urls import reverse
 
 
-@tag('integration')
+@tag('integration', 'access-control')
 class TestAccessControl(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
@@ -119,6 +119,26 @@ class TestAccessControl(TestCase):
 
         # Deleting course leaders
         response = c.get(reverse('modulesApplication:delete-course-leader', kwargs={
+            'pk': 'not-necessary'
+        }), follow=True)
+        self.assertEqual(403, response.status_code)
+
+        # Viewing course leaders
+        response = c.get(reverse('modulesApplication:staff-view-modules'), follow=True)
+        self.assertEqual(403, response.status_code)
+
+        # Updating course leaders
+        response = c.get(reverse('modulesApplication:staff-update-module', kwargs={
+            'pk': 'not-necessary'
+        }), follow=True)
+        self.assertEqual(403, response.status_code)
+
+        # Creating course leaders
+        response = c.get(reverse('modulesApplication:staff-create-module'), follow=True)
+        self.assertEqual(403, response.status_code)
+
+        # Deleting course leaders
+        response = c.get(reverse('modulesApplication:staff-delete-module', kwargs={
             'pk': 'not-necessary'
         }), follow=True)
         self.assertEqual(403, response.status_code)
