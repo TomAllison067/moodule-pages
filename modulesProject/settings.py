02015e1b776ea32ac64e5504592ccf05c1f95f5b
Keiru <nkeirukaw@gmail.com>
2021-03-21 22:42:55 +0000

    Procfile changes for Heroku deployment

--- modulesProject/settings.py
+++ modulesProject/settings.py
@@ -36,7 +36,7 @@ env = environ.Env(
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', 'sample_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
