c076c00a29e12e43e034df846308427a52acb5b6
TomAllison067 <tomlaurenceallison@gmail.com>
2021-03-24 17:35:38 +0000

    csv documentation

--- modulesApplication/csv/csv_reader.py
+++ modulesApplication/csv/csv_reader.py
@@ -1,4 +1,5 @@
 import csv
+from typing import List
 
 from django.db.models.base import ModelBase
 
@@ -8,16 +9,21 @@ from modulesApplication.database.programme import Programme
 
 class CsvReader:
     """
-    A CsvReader to read in data from a csv file.
-    The CsvReader can read headers from a csv file, as well as rows, and return a list of Model objects according
-    to the type that was read in.
+    A `CsvReader` can read .csv files and convert the data into appropriate data model classes for our database.
+
+    We've tried to be generic, and the ``read_table_partial`` method is probably the most appropriate method
+    for most cases.
     """
 
-    def read_headers(self, filename):
+    def read_headers(self, filename) -> List[str]:
         """
-        Reads the headers of the csv file.
-        :param filename: the path of the file to read.
-        :return: the headers from the csv file.
+        Reads the headers (the first row) of a .csv file and returns it.
+
+        Args:
+            filename: the file to read in.
+
+        Returns:
+            A List[str] of containing the headers in the first row of the .csv file.
         """
         with open(file=filename, newline='', encoding='utf8') as csvfile:
             reader = csv.reader(csvfile, delimiter=',', quotechar='"')
@@ -26,10 +32,16 @@ class CsvReader:
 
     def read_table(self, filepath, model_class: ModelBase):
         """
-        Read ALL HEADERS from a csv file corresponding to one database table into a list of Model objects.
-        :param filepath: the path of the csv file to read.
-        :param model_class: the type of Model to read in
-        :return: A list of Model objects.
+        DEPRECATED!
+
+        Reads a .csv file corresponding to a data model, and builds a list of the corresponding model objects.
+
+        Args:
+            filepath: the file to read.
+            model_class: the desired model class.
+
+        Returns:
+            A list of model objects.
         """
         result = []
         with open(file=filepath, newline='', encoding='utf8') as csvfile:
@@ -46,9 +58,19 @@ class CsvReader:
     def read_table_partial(self, filepath, model_class):
         """
         Read from a csv file into a model, reading only those headers which the model has as a field and no more.
-        :param filepath: the path of the csv file
-        :param model_class: the target model class
-        :return: a list of model objects
+
+        A more flexible, generic, better version of ``read_table`` that doesn't require your .csv file to have
+        exactly the same number of headers as the target model class.
+
+        For example, our application only has some fields in the `Programme` model compared to the example sqlite
+        database. This method will only read in the appropriate headers. Use this one!
+
+        Args:
+            filepath: the file to read in.
+            model_class: the target model to read in to.
+
+        Returns:
+            A list of appropriate model objects.
         """
         result = []
         with open(file=filepath, newline='', encoding='utf8') as csvfile:
@@ -71,9 +93,13 @@ class CsvReader:
 
     def read_dict(self, file):
         """
-        Read a csv file and return a list of dicts.
-        :param file the csv file to read
-        :return: a list of dicts of header/value pairs
+        Reads a .csv file and converts it into a list of dictionaries.
+
+        Args:
+            file: the file to read
+
+        Returns:
+            A list of [header:value] dictionaries.
         """
         with open(file, newline='', encoding='utf8') as csvfile:
             reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
